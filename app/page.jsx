ac5325b24099c39504eadb466f9de518cc5b1776
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-09-03 12:49:27 +0300

    styling Ui

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,50 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main>
-      <h2> dashboard</h2>
+      <div className="flex justify-center m-2 p-2 text-blue-300">
+      <h2 className="flex justify-center font-bold text-green-500"> Dashboard</h2>
+      <p className=" text-blue-500 m-2 p-2 flex justify-start">
+      lorem Ipsum is simply dummy text of the printing atypesetting industry.
+       Lorem Ipsum has been the industry's
+       standard dummy text ever since the 1500s, when an unknown printer
+        took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap 
+        into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets 
+        containing Lorem Ipsum passages, and more recently with desktop publishing
+         software like Aldus PageMaker including versions of Lorem Ipsum
+      </p>
 
+      </div>
+
+
+    <div className="flex justify-start m-2 p-2 rounded-md ">
+      <div className="flex ">
+      <Link href="/tickets">
+      <button className="bg-green-500 m-2 p-4 text-white rounded-md flex w-[100%] h-[20%]">View Tickets</button>
+       </Link>
+      </div>
+      <div className="flex  m-2 p-2 ">
+        <div className="card">
+          <p className="text-blue-500  text-md  m-2 p-2 ">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it
+         to make a type specimen book. It has survived not only five centuries, 
+         but also the leap into electronic typesetting, remaining essentially unchanged. 
+         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passage.
+
+          </p>
+      
+
+        </div>
+
+      </div>
+
+    </div>
     </main>
     
   )
